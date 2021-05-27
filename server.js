aeffe9a8cff6b2e2ae519f026db4034c1400ad8d
nellson.maia@gmail.com <nellson.maia@gmail.com>
2021-05-27 14:56:58 +0100

    Initial Commit

--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const { join } = require("path");
 
 const app = express();
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.static(join(__dirname, "build")));
+
+app.get(['/*'], function (req, res) {
+  res.sendFile(join(__dirname, 'build', 'index.html'))
+}); 
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
